4efd5da0c2299118339bdfa64a15429794f3ea06
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 18:37:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
  my_oxford_comma = ["english", "united nations","little John"] .join   
  # join the string element together.
  #... Display them
- string_value = items.join ("")
+ string_value = items.join ("english")
  puts string_value
 end
