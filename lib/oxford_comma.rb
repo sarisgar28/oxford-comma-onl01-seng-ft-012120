6e8974a9a10aed0ecb9143ce8c361a310ece3b9c
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 18:41:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
  my_oxford_comma = ["english", "united nations","little John"] 
- items.join ("english")
- puts .join
- 
+ items.join {"0","2"}
+puts .join
+counter = ("0,2") 
 end
